ea98b8c1a89c8e2efd6c6448810f8a404e70288f
Tim Kretschmer <tim@krtschmr.de>
2017-09-25 19:36:09 +0700

    .

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :bundle_command, "/usr/local/bin/bundle"
 env :PATH, ENV['PATH']
 
-job_type :runner,  "cd :path && exec rails runner -e :environment ':task' :output"
+job_type :runner,  "cd :path && bundle exec rails runner -e :environment ':task' :output"
 
 every 1.minute do
   runner "Stat.import!"
