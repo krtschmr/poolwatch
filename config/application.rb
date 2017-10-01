6c83892140f5a214511d896c967b1957ea9f377c
Tim Kretschmer <tim@krtschmr.de>
2017-10-01 17:44:21 +0700

    krung threp

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Poolwatch
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.time_zone = 'Bangkok'
+    config.time_zone = 'Bangkok'
 
   end
 end
